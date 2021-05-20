a1c723a437b7bd435c58c549b4eeae67d392d335
grtlinux <grtlinux@gmail.com>
2021-05-20 16:57:15 +0900

    create 1st

---
+++ ver00.00/KieaKapeSign-01-demo-00/src/main/java/org/tain/KieaKapeSign01Demo00Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaKapeSign01Demo00Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaKapeSign01Demo00Application.class, args);
+	}
+
+}
